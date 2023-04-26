730fb1cf3d6b311030488626fac7925594cd138e
dwlee <zckixw11@gmail.com>
2023-04-26 14:39:41 +0900

    second commit

--- src/main/java/src/controller/HelloController.java
+++ src/main/java/src/controller/HelloController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.SpringServletContainerInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.ViewResolver;
 import src.service.HelloService;
 
 @Controller
@@ -25,13 +28,14 @@ public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(){
 
-        return "/index.jsp";
+        return "index";
     }
 
     //@GetMapping 사용
     @GetMapping("/hello")
-    public String hello(){
-
-        return "/WEB-INF/views/home.jsp";
+    public String hello(Model model){
+        model.addAttribute("data", "test");
+        return "home";
     }
+
 }
